2b535684387eb7640f0e9d0421143cefa7a9fb91
hokekyo1210 <hokekyo1210@gmail.com>
2018-10-26 19:18:47 +0900

    add reply system

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function getUserData(userID){
 }
 
 async function getUserDataFromMongoDB(userID){
-    const db = await MongoClient.connect(mongodbAddress);
+    const db = await MongoClient.connect(mongodbURI);
     const MyCollection = db.collection('users');
     const result = await MyCollection.find({'userID': userID}).toArray();
     return result;
